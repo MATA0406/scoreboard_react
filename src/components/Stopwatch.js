5331754e7a43864af19dfca84a66db8bec66adb2
MATA0406 <jinho_46@naver.com>
2019-03-02 12:04:53 +0900

    11

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -3,6 +3,17 @@ import React from 'react';
 export class Stopwatch extends React.Component {
   constructor(props){
     super(props);
+    this.state = {
+      isRunning: false
+    }
+  }
+
+  handleStopwatch = (e) => {
+    // 이전 상태를 받아서 isRunning 상태를 토글하는 상태변경 코드 작성
+    this.setState(prevState => ({
+        isRunning: prevState.isRunning
+      })
+    )
   }
 
   render() {
